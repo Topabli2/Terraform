753a94ad6049007a5b8f95ca7605b8662897e4a1
topabli2 <pabloverat2@gmail.com>
2025-03-08 02:46:11 -0300

    key par add

--- practica6/ec2.tf
+++ practica6/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "public_instance" {
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  key_name      = data.aws_key_pair.key
+  key_name      = data.aws_key_pair.key.key_name
 }
 
 
