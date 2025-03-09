67cb167b325c3e9719ee19f25a32bb7996257770
topabli2 <pabloverat2@gmail.com>
2025-03-09 18:57:41 -0300

    add lifecycle commands to practice

--- practica6/ec2.tf
+++ practica6/ec2.tf
@@ -2,7 +2,18 @@ resource "aws_instance" "public_instance" {
   ami           = "ami-08b5b3a93ed654d19"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
-  key_name      = data.aws_key_pair.key.key_name
+  # key_name      = data.aws_key_pair.key.key_name
+
+  # lifecycle {
+  #   create_before_destroy = true 
+  # }   
+  #Esta clausula permite que antes de que terraform destruya esta instancia para volver 
+  #a crearla en otra subnet, la crea primero en esa subner y luego borra la otra
+
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+  #Evita que se elimine este recuros
 }
 
 
