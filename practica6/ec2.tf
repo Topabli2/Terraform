4485a1c8788d8819f2539ac2ea28d76d48facb6b
topabli2 <pabloverat2@gmail.com>
2025-03-10 00:59:09 -0300

    add provisioners variables and scripts

--- practica6/ec2.tf
+++ practica6/ec2.tf
@@ -1,9 +1,33 @@
 resource "aws_instance" "public_instance" {
-  ami                    = "ami-08b5b3a93ed654d19"
-  instance_type          = "t2.micro"
+  ami                    = var.ec2_specs.ami
+  instance_type          = var.ec2_specs.instance_type
   subnet_id              = aws_subnet.public_subnet.id
   key_name               = data.aws_key_pair.key.key_name
   vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
+  user_data              = file("scripts/userdata.sh")
+
+  provisioner "local-exec" {
+    command = "echo instancia creada con IP ${aws_instance.public_instance.public_ip} >> datos_instancia.txt"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo instancia ${self.public_ip} destruida >> datos_instancia.txt"
+  } #self hace referencia al recurso en el que estamos codeando
+
+
+  # Esto no se usa en el mundo real pero se debe saber de igual manera para el examen 
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "echo 'hola mundo' > ~/saludo.txt"
+  #   ]
+  #   connection {
+  #     type        = "ssh"
+  #     host        = self.public_ip
+  #     user        = "ec2-user"
+  #     private_key = file("C:\\Users\\Pablo Vera\\.ssh\\test.pem")
+  #   }
+  # }
 
   # lifecycle {
   #   create_before_destroy = true 
@@ -31,5 +55,3 @@ resource "aws_instance" "public_instance" {
   # }
   # Dispara un mensaje cuando se hace algun cambio al recurso selañalado
 }
-
-
