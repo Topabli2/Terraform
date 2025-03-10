ce50c62394157995e433a47229f23bfe126bd601
topabli2 <pabloverat2@gmail.com>
2025-03-09 23:52:13 -0300

    ADDING IG, RT, SG IN VPC

--- practica6/ec2.tf
+++ practica6/ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "public_instance" {
-  ami           = "ami-08b5b3a93ed654d19"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
-  # key_name      = data.aws_key_pair.key.key_name
+  ami                    = "ami-08b5b3a93ed654d19"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_subnet.id
+  key_name               = data.aws_key_pair.key.key_name
+  vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
 
   # lifecycle {
   #   create_before_destroy = true 
@@ -28,7 +29,7 @@ resource "aws_instance" "public_instance" {
   #     replace_triggered_by
   #    ]
   # }
-  # Dispara un mensaje cuando se hace algun cambio al recurso señalado
+  # Dispara un mensaje cuando se hace algun cambio al recurso selañalado
 }
 
 
